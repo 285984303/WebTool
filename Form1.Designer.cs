f6523e40a1c4f2b3e0b836ecaa130d45ee8609e9
msnqq123456 <285984303@qq.com>
2019-03-05 14:48:16 +0800

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WebTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.txtHTML = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "网址:";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(78, 27);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(554, 21);
+            this.txtUrl.TabIndex = 1;
+            this.txtUrl.Text = "http://140.143.197.142/";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(662, 24);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "分析";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.BtnOK_Click);
+            // 
+            // txtHTML
+            // 
+            this.txtHTML.Location = new System.Drawing.Point(12, 68);
+            this.txtHTML.Name = "txtHTML";
+            this.txtHTML.Size = new System.Drawing.Size(620, 370);
+            this.txtHTML.TabIndex = 3;
+            this.txtHTML.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtHTML);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "网页小工具1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.RichTextBox txtHTML;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebTool
+{
+    public partial class Form1 : Form
+    {
+        HttpWebRequest httpReq;
+        HttpWebResponse httpResp;
+
+        string strBuff = "";
+        char[] cbuffer = new char[256];
+        int byteRead = 0;
+
+        //string filename = @"e:\url_log.txt";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOK_Click(object sender, EventArgs e)
+        {
+            Uri httpURL = new Uri(this.txtUrl.Text);
+
+            ///HttpWebRequest类继承于WebRequest，并没有自己的构造函数，需通过WebRequest的Creat方法 建立，并进行强制的类型转换 
+            httpReq = (HttpWebRequest)WebRequest.Create(httpURL);
+            ///通过HttpWebRequest的GetResponse()方法建立HttpWebResponse,强制类型转换
+
+            httpResp = (HttpWebResponse)httpReq.GetResponse();
+            ///GetResponseStream()方法获取HTTP响应的数据流,并尝试取得URL中所指定的网页内容
+
+            ///若成功取得网页的内容，则以System.IO.Stream形式返回，若失败则产生ProtoclViolationException错 误。在此正确的做法应将以下的代码放到一个try块中处理。这里简单处理 
+            Stream respStream = httpResp.GetResponseStream();
+
+            ///返回的内容是Stream形式的，所以可以利用StreamReader类获取GetResponseStream的内容，并以
+
+            //StreamReader类的Read方法依次读取网页源程序代码每一行的内容，直至行尾（读取的编码格式：UTF8） 
+            StreamReader respStreamReader = new StreamReader(respStream, Encoding.UTF8);
+
+            byteRead = respStreamReader.Read(cbuffer, 0, 256);
+
+            while (byteRead != 0)
+            {
+                string strResp = new string(cbuffer, 0, byteRead);
+                strBuff = strBuff + strResp;
+                byteRead = respStreamReader.Read(cbuffer, 0, 256);
+            }
+
+            respStream.Close();
+            //txtHTML.Text = strBuff;
+
+            //Regex reg = new Regex(@"(?is)<a[^>]*?href=(['""\s]?)(?<href>[^'""\s]*)\1[^>]*?>");
+            //MatchCollection match = reg.Matches(strBuff);
+
+            //Match mc = Regex.Match(strBuff, @"<a href=""([^>]+?)"">([^<]+?)</a>", RegexOptions.Multiline);
+            //string href = mc.Group[1].Value;
+            //string data = mc.Group[2].Value;
+
+            //foreach (Match m in match)
+            //{
+
+            //    txtHTML.Text += m.Groups["href"].Value + "\r\n";
+            //    Console.WriteLine(m.Groups["href"].Value + "\r\n");
+            //}
+
+            //string text = File.ReadAllText(Environment.CurrentDirectory + "//test.txt", Encoding.GetEncoding("gb2312"));
+            string prttern = "<a(\\s+(href=\"(?<url>([^\"])*)\"|'([^'])*'|\\w+=\"(([^\"])*)\"|'([^'])*'))+>(?<text>(.*?))</a>";
+            var maths = Regex.Matches(strBuff, prttern);
+            //抓取出来写入的文件
+            txtHTML.Text = "";
+            using (FileStream w = new FileStream(Environment.CurrentDirectory + "//url_list.txt", FileMode.Create))
+            {
+
+                for (int i = 0; i < maths.Count; i++)
+                {
+                    byte[] bs = Encoding.UTF8.GetBytes(string.Format("链接地址:{0}, innerhtml:{1}", maths[i].Groups["url"].Value,maths[i].Groups["text"].Value) + "\r\n");
+                    w.Write(bs, 0, bs.Length);
+                    //Console.WriteLine();
+                    txtHTML.Text += maths[i].Groups["text"].Value +" "+ maths[i].Groups["url"].Value + "\r\n";
+                }
+            }
+            //Console.ReadKey();
+
+        }
+    }
+}
